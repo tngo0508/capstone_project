a0921332d3d065baae4586ed9b003f2b0265651d
Thomas Ngo <tngo0508@gmail.com>
2021-01-05 10:52:09 -0800

    organize and start working on django-react

--- ba_analysis/ba_analysis/wsgi.py
+++ ba_analysis/ba_analysis/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mainapp project.
+WSGI config for ba_analysis project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ba_analysis.settings')
 
 application = get_wsgi_application()
---
+++ ba_analysis/stocks/api.py
@@ -0,0 +1,9 @@
+from .models import Stock
+from rest_framework import viewsets, permissions
+from .serializers import StockSerializer
+
+
+class StockViewSet(viewsets.ModelViewSet):
+    queryset = Stock.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StockSerializer
