8e9eca3bd57cff0642c8b31dcc3a3241df4c82ba
Thomas Ngo <tngo0508@gmail.com>
2021-03-14 17:34:42 +0000

    changes on google compute engine

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sslserver',
     'corsheaders',
     'rest_framework',
     'stocks',
